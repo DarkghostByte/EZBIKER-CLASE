ca159482a17cc90fe04acbad932d2ea70545e890
DarkGhostByte <Liadeochavez@gmail.com>
2024-03-12 16:47:36 -0600

    Creacion del modelo y controlador de productos

--- ezbiker/api/routes/api.php
+++ ezbiker/api/routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
+use App\Http\Controllers\ProductsController;
 
 Route::get('/users', [UsersController::class,'index']);
 Route::post('/users', [UsersController::class,'store']);
 
 Route::post('/users/actions/verify', [UsersController::class,'verifyEmail']);
+Route::resource('products', ProductsController::class);
 
